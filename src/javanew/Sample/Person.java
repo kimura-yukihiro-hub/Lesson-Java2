6b3df8a773dde04464ab7c8e91eec4f6916266c2
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-24 10:54:41 +0900

    オブジェクト指向の演習のコード

---
+++ src/javanew/Sample/Person.java
@@ -0,0 +1,37 @@
+package javanew.Sample;
+
+//1. カプセル化問5
+// class Person {
+//	private String name;
+//	private int age;
+//}
+//2. 継承問2
+//class Person {
+//	String name;
+//	int age;
+//}
+//class Student extends Person {
+//	String StudentId;
+//}
+//3. ポリモーフィズム問2
+//class Animal {
+//	void makeSound() {
+//		System.out.println("Some soumd...");
+//	}
+//}
+//
+//class Dog extends Animal {
+//	@Override
+//	void makeSound() {
+//		System.out.println("わんわん!");
+//	}
+//
+//}
+//
+//class Cat extends Animal {
+//	@Override
+//	void makeSound() {
+//		System.out.println("にゃー!");
+//	}
+//
+//}
