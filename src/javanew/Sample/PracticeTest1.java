9e6d414fac30662c2c313234b99baa0c3d873478
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-27 15:06:13 +0900

    小テスト大問1

---
+++ src/javanew/Sample/PracticeTest1.java
@@ -0,0 +1,24 @@
+package javanew.Sample;
+
+public class PracticeTest1 {
+	//問題2
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		//問題3
+		int x = 11;
+		System.out.println("x = " + x);
+		//問題4
+		int y = 13;
+		int z = 17;
+		System.out.println("y = " + y + " z = " + z);
+		//問題5
+		int total = y + z;
+		System.out.println("値:" + total);
+		//問題6
+		final int MAX_COUNT = 6;
+		//問題7
+		int sum = total + MAX_COUNT;
+		System.out.println("sum:" + sum);
+
+	}
+}
